59a5f3cd3251d2cd30a8a1be1d97fa9d5be2572b
uaithalkota <ullasaithalk@gmail.com>
2017-01-29 21:33:42 -0600

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ullasAithalgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ullasAithalgithubioApp
+ */
+angular.module('ullasAithalgithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
