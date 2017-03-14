17e3095353bf21d26e304c8588eebf256c4b8be2
uaithalkota <ullasaithalk@gmail.com>
2017-03-13 23:50:41 -0500

    added course list

--- js/custom/main.js
+++ js/custom/main.js
@@ -1,4 +1,16 @@
 
-    $(document).ready(function(){
+$(document).ready(function(){
+
+
       $('.parallax').parallax();
+
+		// jQuery('.text-lighten-5').addClass("hidden").viewportChecker({
+  //       classToAdd: 'visible animated fadeIn',
+  //       offset: 50
+  //      });
+
+
+
+
+
     });
