9773e094d3f4ef00a67942714a8371a2f86f914f
uaithalkota <ullasaithalk@gmail.com>
2017-03-27 23:40:42 -0500

    made changes to courses link, about me description and major overhaul

--- js/custom/main.js
+++ js/custom/main.js
@@ -3,7 +3,14 @@ $(document).ready(function(){
 
 
       $('.parallax').parallax();
-      
+       $('.scrollspy').scrollSpy();
+        scrollOffset:0
+
+
+        	
+
+
+
       jQuery('.skillbar').each(function(){
 	jQuery(this).find('.skillbar-bar').animate({
 		width:jQuery(this).attr('data-percent')
