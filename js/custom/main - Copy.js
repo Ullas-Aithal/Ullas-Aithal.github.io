d4988dd880ce558d76df51bac8126261ea8eb407
uaithalkota <ullasaithalk@gmail.com>
2017-10-14 22:37:17 -0500

    Made optimizations as suggested by Google Page Insights

---
+++ js/custom/main - Copy.js
@@ -0,0 +1,109 @@
+
+$(document).ready(function(){
+
+
+      $('.parallax').parallax();
+       $('.scrollspy').scrollSpy();
+        scrollOffset:0
+        $('.scrollspy').scrollSpy();
+          $('.button-collapse').sideNav({
+      menuWidth: 150, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+
+        var options = [
+      {selector: '#projectDescriptionTab', offset: 620, callback: function(el) {
+        Materialize.toast("Click on the images for project description", 3500 );
+       } },
+       {selector: '#skillSetTab', offset: 20, callback: function(el) {
+        $( "skillSet1" ).removeClass( "skillbar" );
+       } }
+    ];
+    Materialize.scrollFire(options);
+
+    $('#intro-scroll').hover(
+
+      function()
+      {
+        $('#intro-scroll-label').toggleClass("invisible");
+        $('#intro-scroll-label').addClass("animated bounceIn");
+      }
+
+      );
+
+    $('#exp-scroll').hover(
+
+      function()
+      {
+        $('#exp-scroll-label').toggleClass("invisible");
+        $('#exp-scroll-label').addClass("animated bounceIn");
+      }
+
+      );
+
+    $('#edu-scroll').hover(
+
+      function()
+      {
+        $('#edu-scroll-label').toggleClass("invisible");
+        $('#edu-scroll-label').addClass("animated bounceIn");
+      }
+
+      );
+
+    $('#prj-scroll').hover(
+
+      function()
+      {
+        $('#prj-scroll-label').toggleClass("invisible");
+        $('#prj-scroll-label').addClass("animated bounceIn");
+      }
+
+      );
+
+    $('#skills-scroll').hover(
+
+      function()
+      {
+        $('#skills-scroll-label').toggleClass("invisible");
+        $('#skills-scroll-label').addClass("animated bounceIn");
+      }
+
+      );
+
+      var waypoint = new Waypoint({
+        element: document.getElementById('intro'),
+        handler: function() {
+          $('#scrollWindow').toggleClass("invisible");
+          $('#scroll-down').toggleClass("invisible");
+          $('#scrollWindow').addClass("animated slideInRight");
+
+        },
+        offset: '65%'
+      });
+
+      var waypoint2 = new Waypoint({
+        element: document.getElementById('skillSetTab'),
+        handler: function() {
+          jQuery('.skillbar').each(function(){
+            jQuery(this).find('.skillbar-bar').animate({
+              width:jQuery(this).attr('data-percent')
+        },2000);
+      });
+
+    
+  },
+  offset: '95%'
+});
+
+     
+
+
+
+
+      
+
+    });
